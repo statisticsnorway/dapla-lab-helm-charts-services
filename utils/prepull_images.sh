7f1af553e02763d522561aecc9ed3eb076626ab2
John Kasper Svergja <johnkasper.svergja@ssb.no>
2023-12-21 10:09:52 +0100

    Bump version of vscode image

--- utils/prepull_images.sh
+++ utils/prepull_images.sh
@@ -7,7 +7,7 @@ pip install kubernetes
 
 KUBERNETES_NAMESPACE=`cat /var/run/secrets/kubernetes.io/serviceaccount/namespace`
 
-# IMAGES_TO_PREPULL='inseefrlab/onyxia-vscode-python:py3.10.9'
+# IMAGES_TO_PREPULL='inseefrlab/onyxia-vscode-python:py3.10.13'
 python prepull_images.py $KUBERNETES_NAMESPACE $IMAGES_TO_PREPULL
 
 kubectl delete deployments.apps --ignore-not-found=true prepull
